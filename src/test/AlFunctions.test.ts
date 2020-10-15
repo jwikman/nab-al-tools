a2d6a9976a3eb95fdfb2cb7ebe1fef4bcff1865b
theschitz <theodore@theschitz.se>
2020-10-15 21:52:56 +0200

    remove failing test and clean up of green stuff.

--- src/test/AlFunctions.test.ts
+++ src/test/AlFunctions.test.ts
@@ -1,26 +1,10 @@
-//
-// Note: This example test is leveraging the Mocha test framework.
-// Please refer to their documentation on https://mochajs.org/ for help.
-//
-
-// The module 'assert' provides assertion methods from node
 import * as assert from 'assert';
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
-// import * as vscode from 'vscode';
 import * as AlFunctions from '../AlFunctions';
 import * as ALObject from '../ALObject';
 import * as ALObjectTestLibrary from './ALObjectTestLibrary';
 
-
-// Defines a Mocha test suite to group tests of similar kind together
 suite("AL Functions Tests", function () {
-    test("THIS FUNCTION SHOULD FAIL", function () {
-        assert.equal(1,2,'This should make Run Tests/ build fail');
-    });
 
-    // Defines a Mocha unit test
     test("Valid Object Desriptors", function () {
         let objectDescriptorArr = ALObjectTestLibrary.getValidObjectDescriptors();
         for (let index = 0; index < objectDescriptorArr.length; index++) {
