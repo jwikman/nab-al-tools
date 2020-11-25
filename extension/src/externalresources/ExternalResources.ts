de1fb6c2c388124673f6965feb26d98c8c190e01
theschitz <theodore@theschitz.se>
2020-11-25 11:25:36 +0100

    nicer constructor

--- extension/src/externalresources/ExternalResources.ts
+++ extension/src/externalresources/ExternalResources.ts
@@ -12,8 +12,10 @@ interface ExternalResourceInterface {
 }
 
 interface BlobContainerInterface {
+    baseUrl: string;
     blobs: ExternalResource[];
     exportPath: string;
+    sasToken: string;
     getBlobs(filter: string[]|undefined): void;
     addBlob(name: string, uri: string): void;
 }
@@ -24,8 +26,8 @@ export class ExternalResource implements ExternalResourceInterface {
     data: undefined;
 
     constructor(name: string, uri: string,) {
-        this.uri = uri;
         this.name = name;
+        this.uri = uri;
     }
 
     public async get(writeStream: WriteStream): Promise<string> {
@@ -49,11 +51,15 @@ export class ExternalResource implements ExternalResourceInterface {
 }
 
 export class BlobContainer implements BlobContainerInterface {
+    baseUrl: string;
     blobs: ExternalResource[] = [];
+    sasToken: string;
     exportPath: string;
 
-    constructor(exportPath: string) {
+    constructor(exportPath: string, baseUrl: string, sasToken: string) {
+        this.baseUrl = baseUrl;
         this.exportPath = exportPath;
+        this.sasToken = sasToken;
     }
 
     public getBlobs(languageCodeFilter?: string[]) {
@@ -78,11 +84,16 @@ export class BlobContainer implements BlobContainerInterface {
         });
     }
 
-    public addBlob(name: string, uri: string): void {
-        this.blobs.push(new ExternalResource(name, uri));
+    public addBlob(name: string): void {
+        let uri = this.url(name);
+        this.blobs.push(new ExternalResource(name, uri.toString()));
     }
 
     public getBlobByName(name: string): ExternalResource {
         return this.blobs.filter(b => b.name === name)[0];
+    }
+
+    public url(name: string): URL {
+        return new URL(`${this.baseUrl}${name}?${this.sasToken}`);
     }
 }
