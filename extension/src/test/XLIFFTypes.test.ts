78295c6465c377c437c958585a30142a756a0045
theschitz <theodore@theschitz.se>
2020-12-01 09:25:38 +0100

    remove unused import

--- extension/src/test/XLIFFTypes.test.ts
+++ extension/src/test/XLIFFTypes.test.ts
@@ -1,5 +1,4 @@
 import * as assert from 'assert';
-import * as fs from 'fs';
 import * as path from 'path';
 
 import { Xliff, TransUnit, Target, Note, TargetState, SizeUnit } from '../XLIFFDocument';
