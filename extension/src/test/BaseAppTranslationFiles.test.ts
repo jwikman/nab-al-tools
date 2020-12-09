53408d684e4afe5ebdf4c9b18c3b9117da19b20f
theschitz <theodore@theschitz.se>
2020-12-09 17:00:23 +0100

    test for null and undefined

--- extension/src/test/BaseAppTranslationFiles.test.ts
+++ extension/src/test/BaseAppTranslationFiles.test.ts
@@ -1,4 +1,5 @@
 import * as assert from 'assert';
+import { isNullOrUndefined } from 'util';
 import * as BaseAppTranslationFiles from '../externalresources/BaseAppTranslationFiles';
 
 suite("Base App Translation Files Tests", function () {
@@ -9,6 +10,7 @@ suite("Base App Translation Files Tests", function () {
 
     test("localTranslationFiles", function () {
         const localTranslationFiles = BaseAppTranslationFiles.localBaseAppTranslationFiles();
+        assert.equal(isNullOrUndefined(localTranslationFiles), false, 'map should not be null or undefined');
         assert.notEqual(localTranslationFiles.size, 0, 'Unexpected Map size');
     });
 });
