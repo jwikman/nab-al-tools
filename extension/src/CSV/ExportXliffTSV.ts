45e0265a139fe9a449041e07142cc80b614f675a
theschitz <theodore@theschitz.se>
2021-03-28 18:47:53 +0200

    clean up

--- extension/src/CSV/ExportXliffTSV.ts
+++ extension/src/CSV/ExportXliffTSV.ts
@@ -1,34 +1,35 @@
 import { isNullOrUndefined } from "util";
-import { Xliff } from "../XLIFFDocument";
+import { CustomNoteType, Xliff } from "../XLIFFDocument";
 import { CSV } from "./CSV";
+
 
 export function createXliffTSV(xlf: Xliff): CSV {
     let csv = new CSV();
     csv.headers =
         [
-            "Id"
-            , "Source"
-            , "Target"
-            , "Developer Note"
-            , "Max Length"
-            , "Comment"
-            , "Xliff Generator Note"
-            , "NAB AL Tool Note"
-            , "State"
-            , "State Qualifier"
+            "Id",
+            "Source",
+            "Target",
+            "Developer Note",
+            "Max Length",
+            "Comment",
+            "Xliff Generator Note",
+            CustomNoteType.RefreshXlfHint,
+            "State",
+            "State Qualifier"
         ];
     xlf.transunit.forEach(tu => {
         csv.addLine([
-            tu.id
-            , tu.source
-            , tu.targetTextContent
-            , tu.getNoteTextContent("Developer Note")
-            , isNullOrUndefined(tu.maxwidth) ? "" : tu.maxwidth.toString()
-            , "" // comment
-            , tu.getNoteTextContent("Xliff Generator Note")
-            , tu.getNoteTextContent("NAB AL Tool Note")
-            , tu.targetState
-            , tu.targetStateQualifier
+            tu.id,
+            tu.source,
+            tu.targetTextContent,
+            tu.getNoteTextContent("Developer"),
+            isNullOrUndefined(tu.maxwidth) ? "" : tu.maxwidth.toString(),
+            "", // comment
+            tu.getNoteTextContent("Xliff Generator"),
+            tu.getNoteTextContent(CustomNoteType.RefreshXlfHint),
+            tu.targetState,
+            tu.targetStateQualifier
         ]);
     });
     return csv;
