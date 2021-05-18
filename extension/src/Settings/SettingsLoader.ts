308fb8e6ad49219664e00d56a1506c15d43db50a
jwikman <jwikman@users.noreply.github.com>
2021-05-19 00:58:36 +0200

    Load settings dynamically

--- extension/src/Settings/SettingsLoader.ts
+++ extension/src/Settings/SettingsLoader.ts
@@ -1,6 +1,7 @@
 import { join } from "path";
 import * as fs from "fs";
 import * as vscode from "vscode";
+import * as stripJsonComments from "strip-json-comments";
 import {
   AppManifest,
   LaunchSettings,
@@ -8,146 +9,23 @@ import {
   IAppManifest,
   ILaunchFile,
 } from "./Settings";
-import * as stripJsonComments from "strip-json-comments";
+import { settingsMap } from "./SettingsMap";
 
 export function getSettings(): Settings {
   const workspaceFolderPath = getWorkspaceFolderPath();
-  const workspaceKey = "NAB";
   const config = vscode.workspace.getConfiguration(
-    workspaceKey,
+    undefined,
     vscode.Uri.file(workspaceFolderPath)
   );
   const settings = new Settings(workspaceFolderPath);
 
-  settings.signToolPath = getConfigValue(
-    config.get("SignToolPath"),
-    settings.signToolPath
-  );
-  settings.signingCertificateName = getConfigValue(
-    config.get("SigningCertificateName"),
-    settings.signingCertificateName
-  );
-  settings.signingTimeStampServer = getConfigValue(
-    config.get("SigningTimeStampServer"),
-    settings.signingTimeStampServer
-  );
-  settings.powerShellWithDocker = getConfigValue(
-    config.get("PowerShellWithDocker"),
-    settings.powerShellWithDocker
-  );
-  settings.showXlfHighlights = getConfigValue(
-    config.get("ShowXlfHighlights"),
-    settings.showXlfHighlights
-  );
-  settings.xlfHighlightsDecoration = getConfigValue(
-    config.get("XlfHighlightsDecoration"),
-    settings.xlfHighlightsDecoration
-  );
-  settings.useExternalTranslationTool = getConfigValue(
-    config.get("UseExternalTranslationTool"),
-    settings.useExternalTranslationTool
-  );
-  settings.detectInvalidTargets = getConfigValue(
-    config.get("DetectInvalidTargets"),
-    settings.detectInvalidTargets
-  );
-  settings.useDTS = getConfigValue(config.get("UseDTS"), settings.useDTS);
-  settings.dtsProjectId = getConfigValue(
-    config.get("DTS ProjectId"),
-    settings.dtsProjectId
-  );
-  settings.setDtsExactMatchToState = getConfigValue(
-    config.get("Exact Match To State"),
-    settings.setDtsExactMatchToState
-  );
-  settings.replaceSelfClosingXlfTags = getConfigValue(
-    config.get("ReplaceSelfClosingXlfTags"),
-    settings.replaceSelfClosingXlfTags
-  );
-  settings.searchOnlyXlfFiles = getConfigValue(
-    config.get("SearchOnlyXlfFiles"),
-    settings.searchOnlyXlfFiles
-  );
-  settings.matchTranslation = getConfigValue(
-    config.get("MatchTranslation"),
-    settings.matchTranslation
-  );
-  settings.matchBaseAppTranslation = getConfigValue(
-    config.get("MatchBaseAppTranslation"),
-    settings.matchBaseAppTranslation
-  );
-  settings.translationSuggestionPaths = getConfigValue(
-    config.get("TranslationSuggestionPaths"),
-    settings.translationSuggestionPaths
-  );
-  settings.consoleLogOutput = getConfigValue(
-    config.get("ConsoleLogOutput"),
-    settings.consoleLogOutput
-  );
-  settings.loadSymbols = getConfigValue(
-    config.get("LoadSymbols"),
-    settings.loadSymbols
-  );
-  settings.tooltipDocsIgnorePageExtensionIds = getConfigValue(
-    config.get("TooltipDocsIgnorePageExtensionIds"),
-    settings.tooltipDocsIgnorePageExtensionIds
-  );
-  settings.tooltipDocsIgnorePageIds = getConfigValue(
-    config.get("TooltipDocsIgnorePageIds"),
-    settings.tooltipDocsIgnorePageIds
-  );
-  settings.tooltipDocsFilePath = getConfigValue(
-    config.get("TooltipDocsFilePath"),
-    settings.tooltipDocsFilePath
-  );
-  settings.generateTooltipDocsWithExternalDocs = getConfigValue(
-    config.get("GenerateTooltipDocsWithExternalDocs"),
-    settings.generateTooltipDocsWithExternalDocs
-  );
-  settings.generateDeprecatedFeaturesPageWithExternalDocs = getConfigValue(
-    config.get("GenerateDeprecatedFeaturesPageWithExternalDocs"),
-    settings.generateDeprecatedFeaturesPageWithExternalDocs
-  );
-  settings.ignoreTransUnitInGeneratedDocumentation = getConfigValue(
-    config.get("IgnoreTransUnitInGeneratedDocumentation"),
-    settings.ignoreTransUnitInGeneratedDocumentation
-  );
-  settings.docsRootPath = getConfigValue(
-    config.get("DocsRootPath"),
-    settings.docsRootPath
-  );
-  settings.removeObjectNamePrefixFromDocs = getConfigValue(
-    config.get("RemoveObjectNamePrefixFromDocs"),
-    settings.removeObjectNamePrefixFromDocs
-  );
-  settings.docsIgnorePaths = getConfigValue(
-    config.get("DocsIgnorePaths"),
-    settings.docsIgnorePaths
-  );
-  settings.createTocFilesForDocs = getConfigValue(
-    config.get("CreateTocFilesForDocs"),
-    settings.createTocFilesForDocs
-  );
-  settings.includeTablesAndFieldsInDocs = getConfigValue(
-    config.get("IncludeTablesAndFieldsInDocs"),
-    settings.includeTablesAndFieldsInDocs
-  );
-  settings.createInfoFileForDocs = getConfigValue(
-    config.get("CreateInfoFileForDocs"),
-    settings.createInfoFileForDocs
-  );
-  settings.createUidForDocs = getConfigValue(
-    config.get("CreateUidForDocs"),
-    settings.createUidForDocs
-  );
-  settings.xliffCSVExportPath = getConfigValue(
-    config.get("Xliff CSV Export Path"),
-    settings.xliffCSVExportPath
-  );
-  settings.xliffCSVImportTargetState = getConfigValue(
-    config.get("CSV Import Target State"),
-    settings.xliffCSVImportTargetState
-  );
+  settingsMap.forEach((value, key) => {
+    const configuredValue = config.get(key);
+    if (configuredValue !== undefined) {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      (<any>settings)[value] = configuredValue;
+    }
+  });
 
   return settings;
 }
@@ -192,13 +70,6 @@ function loadJson(filePath: string): unknown {
   }
   const json = JSON.parse(stripJsonComments(fileContent));
   return json;
-}
-
-function getConfigValue<T>(configValue: T | undefined, defaultValue: T): T {
-  if (configValue === undefined) {
-    return defaultValue;
-  }
-  return configValue;
 }
 
 function getWorkspaceFolderPath(): string {
---
+++ extension/src/Settings/SettingsMap.ts
@@ -0,0 +1,52 @@
+// import * as Settings from './Settings';
+
+import { Settings } from "./Settings";
+
+// Setting -> property on the Settings class
+export const settingsMap = new Map<string, keyof Settings>([
+  ["NAB.SignToolPath", "signToolPath"],
+  ["NAB.SigningCertificateName", "signingCertificateName"],
+  ["NAB.SigningTimeStampServer", "signingTimeStampServer"],
+  ["NAB.PowerShellWithDocker", "powerShellWithDocker"],
+  ["NAB.ShowXlfHighlights", "showXlfHighlights"],
+  ["NAB.XlfHighlightsDecoration", "xlfHighlightsDecoration"],
+  ["NAB.UseExternalTranslationTool", "useExternalTranslationTool"],
+  ["NAB.DetectInvalidTargets", "detectInvalidTargets"],
+  ["NAB.UseDTS", "useDTS"],
+  ["NAB.DTS ProjectId", "dtsProjectId"],
+  ["NAB.Exact Match To State", "setDtsExactMatchToState"],
+  ["NAB.ReplaceSelfClosingXlfTags", "replaceSelfClosingXlfTags"],
+  ["NAB.SearchOnlyXlfFiles", "searchOnlyXlfFiles"],
+  ["NAB.MatchTranslation", "matchTranslation"],
+  ["NAB.MatchBaseAppTranslation", "matchBaseAppTranslation"],
+  ["NAB.TranslationSuggestionPaths", "translationSuggestionPaths"],
+  ["NAB.ConsoleLogOutput", "consoleLogOutput"],
+  ["NAB.LoadSymbols", "loadSymbols"],
+  [
+    "NAB.TooltipDocsIgnorePageExtensionIds",
+    "tooltipDocsIgnorePageExtensionIds",
+  ],
+  ["NAB.TooltipDocsIgnorePageIds", "tooltipDocsIgnorePageIds"],
+  ["NAB.TooltipDocsFilePath", "tooltipDocsFilePath"],
+  [
+    "NAB.GenerateTooltipDocsWithExternalDocs",
+    "generateTooltipDocsWithExternalDocs",
+  ],
+  [
+    "NAB.GenerateDeprecatedFeaturesPageWithExternalDocs",
+    "generateDeprecatedFeaturesPageWithExternalDocs",
+  ],
+  [
+    "NAB.IgnoreTransUnitInGeneratedDocumentation",
+    "ignoreTransUnitInGeneratedDocumentation",
+  ],
+  ["NAB.DocsRootPath", "docsRootPath"],
+  ["NAB.RemoveObjectNamePrefixFromDocs", "removeObjectNamePrefixFromDocs"],
+  ["NAB.DocsIgnorePaths", "docsIgnorePaths"],
+  ["NAB.CreateTocFilesForDocs", "createTocFilesForDocs"],
+  ["NAB.IncludeTablesAndFieldsInDocs", "includeTablesAndFieldsInDocs"],
+  ["NAB.CreateInfoFileForDocs", "createInfoFileForDocs"],
+  ["NAB.CreateUidForDocs", "createUidForDocs"],
+  ["NAB.Xliff CSV Export Path", "xliffCSVExportPath"],
+  ["NAB.CSV Import Target State", "xliffCSVImportTargetState"],
+]);
