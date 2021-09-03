ad8c5770ae3187a0f0e414ff04ac195751c48a30
theschitz <theodore@theschitz.se>
2021-09-03 10:24:11 +0200

    add test for validateLocalBaseAppTranslationFiles

--- extension/src/test/BaseAppTranslationFiles.test.ts
+++ extension/src/test/BaseAppTranslationFiles.test.ts
@@ -1,6 +1,8 @@
 import * as assert from "assert";
 import { isNullOrUndefined } from "util";
 import * as BaseAppTranslationFiles from "../externalresources/BaseAppTranslationFiles";
+import * as path from "path";
+import * as fs from "fs";
 
 suite("Base App Translation Files Tests", function () {
   const TIMEOUT = 360000;
@@ -32,5 +34,34 @@ suite("Base App Translation Files Tests", function () {
       "map should not be null or undefined"
     );
     assert.notEqual(localTranslationFiles.size, 0, "Unexpected Map size");
+  });
+
+  test.only("validateLocalBaseAppTranslationFiles", async function () {
+    //TODO: remove only
+    this.timeout(TIMEOUT); // Just in case there's a lot of files in the folder
+    const filePath = path.resolve(__dirname, "../../out/externalresources/");
+    fs.writeFileSync(
+      path.resolve(filePath, "da-dk.json"),
+      `{ "invalid": "Jay's son`
+    );
+    fs.writeFileSync(path.resolve(filePath, "sv-se.json"), `{"valid": "JSON"}`);
+
+    assert.deepStrictEqual(
+      await BaseAppTranslationFiles.validateLocalBaseAppTranslationFiles(),
+      1,
+      "Unexpected number of files deleted in first run."
+    );
+
+    assert.deepStrictEqual(
+      await BaseAppTranslationFiles.validateLocalBaseAppTranslationFiles(),
+      0,
+      "No files should have been deleted in second run."
+    );
+
+    assert.deepStrictEqual(
+      fs.existsSync(path.resolve(filePath, "sv-se.json")),
+      true,
+      "Expected to find file. To many may have been delted."
+    );
   });
 });
