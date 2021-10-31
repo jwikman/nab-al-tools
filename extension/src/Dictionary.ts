abdb37a5c5da3222a3ad8cba33f7e2b490c5764a
theschitz <theodore@theschitz.se>
2021-10-31 14:14:10 +0100

    class and interface

---
+++ extension/src/Dictionary.ts
@@ -0,0 +1,75 @@
+import { existsSync, readFileSync, writeFileSync } from "fs";
+
+export class Dictionary implements IDictonary {
+  language: string;
+  wordList: DictPair[];
+  dictionaryFile: string;
+
+  constructor(dictionaryFile: string) {
+    this.dictionaryFile = dictionaryFile;
+    if (!existsSync(dictionaryFile)) {
+      throw new Error(`Could not open file: "${this.dictionaryFile}"`);
+    }
+    const d = JSON.parse(
+      readFileSync(this.dictionaryFile, "utf8")
+    ) as IDictonary;
+    this.language = d.language;
+    this.wordList = d.wordList;
+  }
+
+  addWord(word: string, replaceWith: string): Dictionary {
+    const newWord: DictPair = {
+      word: word.toLocaleLowerCase(),
+      replacement: replaceWith,
+      setting: this.defaultSetting(),
+    };
+    this.wordList.push(newWord);
+    return this;
+  }
+
+
+  defaultSetting(): WordSetting {
+    return {
+      matchWholeWord: true,
+      matchCasing: true,
+      useRegex: false,
+      keepCasingOnFirstCharacter: true,
+    };
+  }
+
+  static newDictionary(
+    path: string,
+    language: string,
+    suffix?: string
+  ): Dictionary {
+    const fileName = `${path}/${language}${
+      suffix === undefined ? "" : `.${suffix}`
+    }.json`;
+    writeFileSync(
+      fileName,
+      JSON.stringify({ language: language, words: [] }),
+      "utf8"
+    );
+    const dict = new Dictionary(fileName);
+    dict.dictionaryFile = fileName;
+    return dict;
+  }
+}
+
+interface IDictonary {
+  language: string;
+  wordList: DictPair[];
+}
+
+interface DictPair {
+  word: string;
+  replacement: string;
+  setting: WordSetting;
+}
+
+interface WordSetting {
+  matchWholeWord: boolean;
+  matchCasing: boolean;
+  useRegex: boolean;
+  keepCasingOnFirstCharacter: boolean;
+}
