3ea9e9f0bcd0de93e4aeb9fd592801a4ac685144
theschitz <theodore@theschitz.se>
2021-10-31 15:37:01 +0100

    add test for static function keepCasingOnFirstChar

--- extension/src/test/Dictionary.test.ts
+++ extension/src/test/Dictionary.test.ts
@@ -5,16 +5,7 @@ import { Dictionary } from "../Dictionary";
 suite("Dictionary Tests", () => {
   const existingDict = `${__dirname}/../../src/test/resources/sv-se.dts.json`;
   const resourcesDir = __dirname;
-  test("Dictionary.newDictionary", function () {
-    const dict = Dictionary.newDictionary(resourcesDir, "da-dk", "dts");
-    assert.ok(
-      existsSync(dict.dictionaryFile),
-      "New dictionary was expected to be created."
-    );
-    unlinkSync(dict.dictionaryFile);
-  });
-
-  test.only("Use Existing Dictionary", function () {
+  test.only("Dictionary integration", function () {
     const dict = new Dictionary(existingDict);
     assert.strictEqual(
       dict.translate("Kontrakt"),
@@ -26,5 +17,19 @@ suite("Dictionary Tests", () => {
       "avtal",
       "Expected casing to be kept for first character."
     );
+  });
+
+  // Static functions
+  test("Dictionary.keepCasingOnFirstChar()", function () {
+    Dictionary.keepCasingOnFirstChar("word", "Replacement");
+  });
+
+  test("Dictionary.newDictionary", function () {
+    const dict = Dictionary.newDictionary(resourcesDir, "da-dk", "dts");
+    assert.ok(
+      existsSync(dict.dictionaryFile),
+      "New dictionary was expected to be created."
+    );
+    unlinkSync(dict.dictionaryFile);
   });
 });
