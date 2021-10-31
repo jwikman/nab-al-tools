715b89141297df0987767eea13d192afef47371f
theschitz <theodore@theschitz.se>
2021-10-31 22:57:54 +0100

    split up unit tests

--- extension/src/test/Dictionary.test.ts
+++ extension/src/test/Dictionary.test.ts
@@ -5,42 +5,64 @@ import { Dictionary } from "../Dictionary";
 suite("Dictionary Tests", () => {
   const existingDict = `${__dirname}/../../src/test/resources/sv-se.dts.json`;
   const resourcesDir = __dirname;
-  test.only("Dictionary integration tests", function () {
-    const dict = new Dictionary(existingDict);
+  const leDict = new Dictionary(existingDict);
+  test.only("Dictionary.addWord()", function () {
     assert.strictEqual(
-      dict.wordList.length,
+      leDict.wordList.length,
       1,
       "Unexpected length of wordList"
     );
-    dict.addWord("Satan", "Beelzebub");
+    leDict.addWord("Satan", "Beelzebub");
     assert.strictEqual(
-      dict.wordList.length,
+      leDict.wordList.length,
       2,
       "Unexpected length of wordList"
     );
+  });
+
+  test("Dictionary.searchAndReplace()", function () {
+    leDict.wordList[0].settings.matchCasing = false;
+    translationTestList().forEach((t) => {
+      assert.strictEqual(
+        leDict.searchAndReplace(t.actual),
+        t.expected,
+        `Expected translation ${t.actual} => ${t.expected}`
+      );
+    });
+    const translateText = {
+      actual: `The quick brown fox hade ett kontrakt. Lata hunden åkte dit för kontraktsbrott. Hunden var snabb som Satan!`,
+      expected: `The quick brown fox hade ett avtal. Lata hunden åkte dit för kontraktsbrott. Hunden var snabb som Beelzebub!`,
+    };
     assert.strictEqual(
-      dict.translate("Satan"),
+      leDict.searchAndReplace(translateText.actual),
+      translateText.expected,
+      "Translated result unexpected"
+    );
+  });
+
+  test.only("Dictionary.translate()", function () {
+    assert.strictEqual(
+      leDict.translate("Satan"),
       "Beelzebub",
       "The air smells of sulfur and you hear the sound of hoofs approaching from behind."
     );
     assert.strictEqual(
-      dict.translate("Kontrakt"),
+      leDict.translate("Kontrakt"),
       "Avtal",
       "Expected word to be translated."
     );
     assert.strictEqual(
-      dict.translate("kontrakt"),
+      leDict.translate("kontrakt"),
       "avtal",
       "Expected casing to be kept for first character."
     );
   });
-
   // Static functions
-  test("Dictionary.keepCasingOnFirstChar()", function () {
+  test.only("Dictionary.keepCasingOnFirstChar()", function () {
     Dictionary.keepCasingOnFirstChar("word", "Replacement");
   });
 
-  test("Dictionary.newDictionary", function () {
+  test.only("Dictionary.newDictionary", function () {
     const dict = Dictionary.newDictionary(resourcesDir, "da-dk", "dts");
     assert.ok(
       existsSync(dict.dictionaryFile),
@@ -49,3 +71,16 @@ suite("Dictionary Tests", () => {
     unlinkSync(dict.dictionaryFile);
   });
 });
+
+function translationTestList(): { actual: string; expected: string }[] {
+  return [
+    {
+      actual: "Kontraktslista",
+      expected: "Kontraktslista",
+    },
+    {
+      actual: "Mina kontrakt",
+      expected: "Mina avtal",
+    },
+  ];
+}
