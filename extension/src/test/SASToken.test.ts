9c8b4b0b84ca28578a06af19ef78c24c0b41dbb0
theschitz <theodore@theschitz.se>
2021-11-25 23:09:17 +0100

    Test current sas token. Warn if < 30 days. Assert > 10 days until expiration

--- extension/src/test/SASToken.test.ts
+++ extension/src/test/SASToken.test.ts
@@ -1,11 +1,29 @@
 import * as assert from "assert";
 import { SASToken } from "../externalresources/SASToken";
+import * as BaseAppTranslationFiles from "../externalresources/BaseAppTranslationFiles";
 
 suite("SASToken tests", function () {
   const expiredToken =
     "sv=2019-12-12&ss=f&srt=o&sp=r&se=2021-11-25T05:28:10Z&st=2020-11-24T21:28:10Z&spr=https&sig=JP3RwQVCZBo16vJCznojVIMvPOHgnDuH937ppzPmEqQ%3D";
   const expiredSASToken = new SASToken(expiredToken);
-
+  const currentToken = new SASToken(
+    BaseAppTranslationFiles.BlobContainerSettings.sasToken
+  );
+  test.only("Current SAS Token Test", function () {
+    /**
+     * Assert that the current SASToken for the blob storage is not about to expire.
+     */
+    const daysUntilExpiration = currentToken.daysUntilExpiration();
+    if (daysUntilExpiration <= 30) {
+      console.warn(
+        `*** WARNING! SAS token expires in ${daysUntilExpiration} days. ***`
+      );
+      assert.ok(
+        daysUntilExpiration > 10,
+        "Less than 10 days until current SAS Token expires. Generate a new token ASAP!"
+      );
+    }
+  });
   test("SASToken.daysUntilExpiration", function () {
     const daysUntilExpiration = expiredSASToken.daysUntilExpiration();
     assert.ok(
