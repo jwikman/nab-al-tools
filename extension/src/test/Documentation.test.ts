ac47780f3a7eeef1efa603de742742907e3b240d
theschitz <theodore@theschitz.se>
2021-12-05 23:45:51 +0100

    finally!

--- extension/src/test/Documentation.test.ts
+++ extension/src/test/Documentation.test.ts
@@ -7,7 +7,6 @@ import * as SettingsLoader from "../Settings/SettingsLoader";
 import * as Common from "../Common";
 
 suite("Documentation Tests", async function () {
-  this.timeout(5000);
   // const WORKFLOW = process.env.GITHUB_ACTION; // Only run in GitHub Workflow
   const WORKFLOW = true;
   const settings = SettingsLoader.getSettings();
@@ -18,11 +17,11 @@ suite("Documentation Tests", async function () {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
   const appPackage = require("../../package.json");
 
-  test.only("Documentation.generateExternalDocumentation", async function () {
+  test("Documentation.generateExternalDocumentation", async function () {
     if (!WORKFLOW) {
       this.skip();
     }
-    this.timeout(5000);
+    this.timeout(10000);
     // remove docs directory
     fs.rmdirSync(tempDocsPath, { recursive: true });
     settings.docsRootPath = tempDocsPath;
@@ -31,6 +30,8 @@ suite("Documentation Tests", async function () {
       settings.tooltipDocsFilePath
     );
     settings.generateTooltipDocsWithExternalDocs = true;
+    settings.ignoreTransUnitInGeneratedDocumentation = [];
+    settings.tooltipDocsIgnorePageIds = [];
     await Documentation.generateExternalDocumentation(settings, appManifest);
 
     assert.ok(
@@ -98,15 +99,7 @@ suite("Documentation Tests", async function () {
     );
 
     testFiles
-      .filter(
-        (f) =>
-          [
-            "info.json",
-            "ToolTips.md",
-            "page-nab-tool-tip-part-1/index.md",
-            "page-nab-tool-tip-part-2/index.md",
-          ].includes(f.relPath) === false
-      )
+      .filter((f) => ["info.json"].includes(f.relPath) === false)
       .forEach((testFile) => {
         const compareFile = compareFiles.find(
           (f) => f.relPath === testFile.relPath
@@ -115,46 +108,18 @@ suite("Documentation Tests", async function () {
           compareFile,
           `Could not find compare file for ${testFile.relPath}`
         );
-        const compare = fs.readFileSync(compareFile.filePath, "utf8");
-        // .replace(/\r/g, "");
-        // .split(/\r\n|\r|\n/);
-        // const test = getLines(fs.readFileSync(testFile.filePath, "utf8"));
-        const test = fs.readFileSync(testFile.filePath, "utf8");
-        // .split(/\r\n|\r|\n/);
-        // assert.deepStrictEqual(
-        //   test,
-        //   compare,
-        //   `Line splitted files are not equal. Relpath "${testFile.relPath}"`
-        // );
-        assert.ok(compare, "Compare text is not ok.");
-        assert.ok(test, "Test text is not ok.");
-        for (let l = 0; l < test.length; l++) {
-          // for (let c = 0; c < test[l].length; c++) {
-          // const testLine = test[l];
-          // const compareLine = compare[l];
-          assert.strictEqual(
-            test.charAt(l),
-            compare.charAt(l),
-            `Found char=${test.charAt(l)} charCode=${test.charCodeAt(
-              l
-            )} at ${l} "${test}" in ${
-              testFile.filePath
-            }.\nExpected char=${compare.charAt(
-              l
-            )} charCode=${compare.charCodeAt(l)} at ${l} "${compare}" in ${
-              compareFile.filePath
-            }`
-          );
-          // }
-        }
+        const expected = fs
+          .readFileSync(compareFile.filePath, "utf8")
+          .split("\r\n");
+        const actual = fs.readFileSync(testFile.filePath, "utf8").split("\r\n");
+        assert.deepStrictEqual(
+          actual,
+          expected,
+          `Unexpected diff found in ${testFile.relPath}`
+        );
       });
   });
 });
-
-// function getLines(content: string): string[] {
-//   content = content.replace(/\r\n/g, "\n");
-//   return content.split("\n");
-// }
 
 function readDirRecursive(
   rootPath: string,
