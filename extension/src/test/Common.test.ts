8186a2fc660e65078f017c29ae644576da6e8954
theschitz <theodore@theschitz.se>
2021-12-14 08:24:03 +0100

    refactor: zipXlfFiles becomes zipFiles in FileFunctions (#232)
    Collect filesystem related functions in FileFunctions.ts

--- extension/src/test/Common.test.ts
+++ extension/src/test/Common.test.ts
@@ -1,13 +1,7 @@
 import * as assert from "assert";
-import * as path from "path";
-import * as fs from "fs";
 import * as Common from "../Common";
 
 suite("Common", function () {
-  const parentPath = path.resolve(__dirname, "common-test");
-  const newPath = path.resolve(parentPath, "new/path/", Common.formatDate());
-  const invalidPathChars = `/?<>\\:*|."`;
-
   test("convertLinefeedToBr", function () {
     assert.strictEqual(
       Common.convertLinefeedToBR("\n\n\n"),
@@ -28,47 +22,6 @@ suite("Common", function () {
       Common.formatDate(d),
       "2001-01-01",
       "Incorrect date string returned"
-    );
-  });
-
-  test("createFolderIfNotExist", function () {
-    assert.strictEqual(
-      fs.existsSync(newPath),
-      false,
-      "Test folder should not exist. This could be an indication that the 'deleteFolderRecursive' test is not working."
-    );
-    Common.createFolderIfNotExist(newPath);
-    assert.ok(fs.existsSync(newPath), "Could not find created folder.");
-
-    // Test error code path
-    if (process.platform === "win32") {
-      // Linux: pretty much allows any character in a path.
-      // MacOS: We're currently not running test on MacOS.
-      let errorMsg = "";
-      try {
-        Common.createFolderIfNotExist(newPath + invalidPathChars);
-      } catch (e) {
-        errorMsg = (e as Error).message;
-      }
-      assert.strictEqual(
-        errorMsg.length > 0,
-        true,
-        "Expected Error to be thrown"
-      );
-    }
-  });
-
-  test("deleteFolderRecursive", function () {
-    Common.deleteFolderRecursive(parentPath);
-    assert.strictEqual(
-      fs.existsSync(newPath),
-      false,
-      "Child path should be deleted"
-    );
-    assert.strictEqual(
-      fs.existsSync(parentPath),
-      false,
-      "Parent path should be deleted."
     );
   });
 });
---
+++ extension/src/test/FileFunctions.test.ts
@@ -0,0 +1,92 @@
+import * as assert from "assert";
+import * as FileFunctions from "../FileFunctions";
+import * as path from "path";
+import * as fs from "fs";
+import * as Common from "../Common";
+
+const WORKFLOW = process.env.GITHUB_ACTION; // Only run in GitHub Workflow
+suite("FileFunctions Tests", function () {
+  const parentPath = path.resolve(__dirname, "filefunctions-test");
+  const newPath = path.resolve(parentPath, "new/path/", Common.formatDate());
+
+  test("zipFiles()", function () {
+    if (!WORKFLOW) {
+      this.skip();
+    }
+    const testAppTranslationsPath = path.join(
+      __dirname,
+      "../../..",
+      "test-app/Xliff-test/Translations/"
+    );
+    const compressFiles: string[] = [
+      path.join(testAppTranslationsPath, "Al.da-dk.xlf"),
+      path.join(testAppTranslationsPath, "Al.g.xlf"),
+      path.join(testAppTranslationsPath, "Al.sv-se.xlf"),
+    ];
+    const expectedFiles = [
+      {
+        name: "Al.da-dk.zip",
+      },
+      {
+        name: "Al.g.zip",
+      },
+      {
+        name: "Al.sv-se.zip",
+      },
+    ];
+    const exportPath = path.join(__dirname, "temp/dts");
+
+    FileFunctions.zipFiles(compressFiles, exportPath);
+    const zipFiles = fs.readdirSync(exportPath, { withFileTypes: true });
+    assert.strictEqual(zipFiles.length, 3, "Unexpected number of zip-files");
+
+    zipFiles.forEach((z) => {
+      assert.ok(
+        expectedFiles.find((zip) => zip.name === z.name),
+        `New file exported ${z.name}. Is this correct?`
+      );
+    });
+  });
+
+  test("createFolderIfNotExist()", function () {
+    const invalidPathChars = `/?<>\\:*|."`;
+    assert.strictEqual(
+      fs.existsSync(newPath),
+      false,
+      "Test folder should not exist. This could be an indication that the 'deleteFolderRecursive' test is not working."
+    );
+    FileFunctions.createFolderIfNotExist(newPath);
+    assert.ok(fs.existsSync(newPath), "Could not find created folder.");
+
+    // Test error code path
+    if (process.platform === "win32") {
+      // Linux: pretty much allows any character in a path.
+      // MacOS: We're currently not running test on MacOS.
+      let errorMsg = "";
+      try {
+        FileFunctions.createFolderIfNotExist(newPath + invalidPathChars);
+      } catch (e) {
+        errorMsg = (e as Error).message;
+      }
+      assert.strictEqual(
+        errorMsg.length > 0,
+        true,
+        "Expected Error to be thrown"
+      );
+    }
+  });
+
+  test("deleteFolderRecursive", function () {
+    FileFunctions.deleteFolderRecursive(parentPath);
+    assert.strictEqual(
+      fs.existsSync(newPath),
+      false,
+      "Child path should be deleted"
+    );
+    assert.strictEqual(
+      fs.existsSync(parentPath),
+      false,
+      "Parent path should be deleted."
+    );
+  });
+});
