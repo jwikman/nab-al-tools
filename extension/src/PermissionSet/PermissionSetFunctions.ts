344c194909da57d82842f5420965507db707719b
Johannes Wikman <jwikman@users.noreply.github.com>
2021-12-30 23:39:01 +0100

    Create Upgrade Codeunit

--- extension/src/PermissionSet/PermissionSetFunctions.ts
+++ extension/src/PermissionSet/PermissionSetFunctions.ts
@@ -61,16 +61,22 @@ export async function startConversion(
               false,
               true
             );
-            await convertToPermissionSet(
+            const folderPath = await convertToPermissionSet(
               manifest,
               alObjects,
               prefix,
               xmlPermissionSets
             );
             vscode.window.showInformationMessage(
-              `PermissionSet objects created and old XML PermissionSets deleted.` // TODO: Upgrade code
+              `PermissionSet objects created, old XML PermissionSets deleted and an upgrade codeunit created.`
             );
-            // TODO: Loop createdPermissionSets and create upgrade codeunit with mapping from oldPermissionSets
+            createUpgradeCodeunit(
+              xmlPermissionSets,
+              prefix,
+              manifest,
+              alObjects,
+              folderPath
+            );
             logger.log("Done: convertToPermissionSet");
             resolve();
           } catch (error) {
@@ -88,11 +94,12 @@ export async function convertToPermissionSet(
   alObjects: ALObject[],
   prefix: string,
   xmlPermissionSets: XmlPermissionSet[]
-): Promise<void> {
+): Promise<string> {
   let lastUsedId = 0;
+  let lastFolderPath = "";
   for (const xmlPermissionSet of xmlPermissionSets) {
     const folderPath = path.parse(xmlPermissionSet.filePath).dir;
-
+    lastFolderPath = folderPath;
     const newPermissionSet: ALPermissionSet = new ALPermissionSet(
       xmlPermissionSet.suggestedNewName,
       xmlPermissionSet.roleName,
@@ -140,6 +147,7 @@ export async function convertToPermissionSet(
       fs.unlinkSync(xmlPermissionSet.filePath);
     }
   }
+  return lastFolderPath;
 }
 
 function getObjectName(
@@ -222,4 +230,88 @@ function getPermissionCasing(character: string, permission: string): string {
     default:
       return "";
   }
+}
+function createUpgradeCodeunit(
+  xmlPermissionSets: XmlPermissionSet[],
+  prefix: string,
+  manifest: AppManifest,
+  alObjects: ALObject[],
+  folderPath: string
+): void {
+  const objectId = getFirstAvailableObjectId(
+    alObjects,
+    manifest,
+    ALObjectType.codeunit,
+    0
+  );
+  const objectName = `${prefix}PermissionSet Upgrade`;
+  const filePath = path.join(folderPath, "PermissionSetUpgrade.codeunit.al");
+  const code = `codeunit ${objectId} "${objectName}"
+{
+    Access = Internal;
+    Subtype = Upgrade;
+
+    trigger OnUpgradePerDatabase()
+    begin
+        if GetDataVersion() <= Version.Create('${manifest.version}') then
+            UpgradePermissionSets();
+    end;
+
+    local procedure UpgradePermissionSets()
+    begin
+        ${xmlPermissionSets
+          .map(
+            (x) =>
+              `UpgradePermissionSet('${x.roleID}', '${x.suggestedNewName}');`
+          )
+          .join("\r\n        ")}
+    end;
+
+    local procedure UpgradePermissionSet(OldPermissionSetCode: Code[20]; NewPermissionSetCode: Code[20])
+    var
+        UserGroupPermissionSet: Record "User Group Permission Set";
+        UserGroupPermissionSet2: Record "User Group Permission Set";
+        AccessControl: Record "Access Control";
+        AccessControl2: Record "Access Control";
+        AppId: Guid;
+        CurrentAppInfo: ModuleInfo;
+    begin
+        NavApp.GetCurrentModuleInfo(CurrentAppInfo);
+        AppId := CurrentAppInfo.Id();
+        UserGroupPermissionSet.SetRange("App ID", AppId);
+        UserGroupPermissionSet.SetRange(Scope, UserGroupPermissionSet.Scope::Tenant);
+        UserGroupPermissionSet.SetRange("Role ID", OldPermissionSetCode);
+        if UserGroupPermissionSet.FindSet() then begin
+            repeat
+                UserGroupPermissionSet2 := UserGroupPermissionSet;
+                UserGroupPermissionSet2.Scope := UserGroupPermissionSet2.Scope::System;
+                UserGroupPermissionSet2."Role ID" := NewPermissionSetCode;
+                UserGroupPermissionSet2.Insert();
+            until UserGroupPermissionSet.Next() = 0;
+            UserGroupPermissionSet.DeleteAll();
+        end;
+
+        AccessControl.SetRange("App ID", AppId);
+        AccessControl.SetRange(Scope, AccessControl.Scope::Tenant);
+        AccessControl.SetRange("Role ID", OldPermissionSetCode);
+        if AccessControl.FindSet() then begin
+            repeat
+                AccessControl2 := AccessControl;
+                AccessControl2.Scope := AccessControl2.Scope::System;
+                AccessControl2."Role ID" := NewPermissionSetCode;
+                AccessControl2.Insert();
+            until AccessControl.Next() = 0;
+            AccessControl.DeleteAll();
+        end;
+    end;
+
+    local procedure GetDataVersion(): Version
+    var
+        ModInfo: ModuleInfo;
+    begin
+        NavApp.GetCurrentModuleInfo(ModInfo);
+        exit(ModInfo.DataVersion());
+    end;
+}`;
+  fs.writeFileSync(filePath, code, { encoding: "utf8" });
 }
