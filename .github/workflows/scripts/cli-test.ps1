01cc8c8653ccf044de9476714c3cdcbd1b333305
Johannes Wikman <jwikman@users.noreply.github.com>
2022-01-17 21:25:14 +0100

    test: Compare output of docs created by CLI

--- .github/workflows/scripts/cli-test.ps1
+++ .github/workflows/scripts/cli-test.ps1
@@ -1,4 +1,9 @@
 $ErrorActionPreference = "continue"
+$CurrentPath = Get-Location
+
+if ((Split-Path -Path $CurrentPath.Path -Leaf).ToLower() -eq "extension" ) {
+    Set-Location (Split-Path -Path $CurrentPath.Path -Parent)
+}
 $cliPath = (Resolve-Path ".\extension\out\cli\CreateDocumentation.js")
 
 $result = node $cliPath  2>&1
@@ -34,9 +39,14 @@ else {
     }
 }
 
+$ErrorActionPreference = "stop"
+
 $TestAppFolderPath = Resolve-Path -Path (Join-Path "." "test-app\Xliff-test")
-$OutputFolderPath = Join-Path ([System.IO.Path]::GetTempPath()) ([Guid]::NewGuid()).ToString()
+$ToolTipExpectedResultFilePath = Resolve-Path -Path (Join-Path $TestAppFolderPath "ToolTips.md")
+$DocsExpectedResultFolderPath = Resolve-Path -Path (Join-Path $TestAppFolderPath "docs")
 $WorkspaceFilePath = Resolve-Path (Join-Path "." "test-app\TestApp.code-workspace")
+
+$OutputFolderPath = Join-Path ([System.IO.Path]::GetTempPath()) ([Guid]::NewGuid()).ToString()
 $ToolTipFilePath = Join-Path ([System.IO.Path]::GetTempPath()) "$(([Guid]::NewGuid()).ToString())\ToolTips.md"
 New-Item -Path $OutputFolderPath -ItemType Directory | Out-Null
 New-Item -Path (Split-Path $ToolTipFilePath -Parent) -ItemType Directory | Out-Null
@@ -46,24 +56,49 @@ node $cliPath $TestAppFolderPath $OutputFolderPath $WorkspaceFilePath $ToolTipFi
 
 # Asserts:
 
-$CompareToolTipFilePath = Resolve-Path -Path (Join-Path "." "test-app\Xliff-test\ToolTips.md")
-# TODO: Function that takes two file paths, compares content
+function Compare-Files {
+    param (
+        $FilePath1,
+        $FilePath2
+    )
+    $Content1 = (Get-Content $FilePath1 -Encoding UTF8)
+    $Content2 = (Get-Content $FilePath2 -Encoding UTF8)
 
-$ToolTipContent = (Get-Content $ToolTipFilePath -Encoding UTF8).TrimEnd().TrimStart().Replace('`r', '').Split('`n')
-$CompareToolTipContent = (Get-Content $CompareToolTipFilePath -Encoding UTF8).TrimEnd().TrimStart().Replace('`r', '').Split('`n')
-
-if ($ToolTipContent.Length -ne $CompareToolTipContent.Length) {
-    throw "Unexpected line count of ToolTip. Current: $($ToolTipContent.Length). Expected: $($CompareToolTipContent.Length)"
-}
-for ($i = 0; $i -lt $ToolTipContent.Count; $i++) {
-    $l1 = $ToolTipContent[$i]
-    $l2 = $CompareToolTipContent[$i]
-    if ($l1 -ne $l2) {
-        throw "Unexpected line content of ToolTip, line $i. Current: '$($l1)'. Expected: '$($l2)'"
+    if ($Content1.Length -ne $Content2.Length) {
+        throw "Unexpected line count of $FilePath1. Current: $($Content1.Length). Expected: $($Content2.Length)"
     }
+    for ($i = 0; $i -lt $Content1.Count; $i++) {
+        $l1 = $Content1[$i]
+        $l2 = $Content2[$i]
+        if ($l1 -ne $l2) {
+            throw "Unexpected line content of file '$FilePath1', line $i.`nCurrent:  '$($l1)'.`nExpected: '$($l2)'"
+        }
+    }
+    Write-Host "File '$(Split-Path $FilePath1 -Leaf)' checked"
 }
 
+Compare-Files -FilePath1 $ToolTipFilePath -FilePath2 $ToolTipExpectedResultFilePath
 
 
-$CompareDocsFolderPath = Resolve-Path -Path (Join-Path "." "test-app\Xliff-test\docs")
-# TODO: Get-ChildItems *.md -recurse - Call above function on all files.
+$DocsFiles = Get-ChildItem -Path $OutputFolderPath -Recurse -Include "*.md", "*.yml"
+$CompareDocsFiles = Get-ChildItem -Path $DocsExpectedResultFolderPath -Recurse -Include "*.md", "*.yml"
+
+
+if ($DocsFiles.Length -ne $CompareDocsFiles.Length) {
+    throw "Unexpected number of files. Current: $($DocsFiles.Length). Expected: $($CompareDocsFiles.Length)"
+}
+
+for ($i = 0; $i -lt $DocsFiles.Count; $i++) {
+    $f1 = $DocsFiles[$i]
+    $f2 = $CompareDocsFiles[$i]
+    $fileName1 = (Split-Path $f1 -Leaf)
+    $fileName2 = (Split-Path $f2 -Leaf)
+    if ($fileName1 -ne $fileName2) {
+        throw "Unexpected filename, file $i. Current: '$($fileName1)'. Expected: '$($fileName2)'"
+    }
+    Compare-Files -FilePath1 $f1.FullName -FilePath2 $f2.FullName
+}
+Remove-Item $OutputFolderPath -Recurse -Force
+Remove-Item $ToolTipFilePath -Force
+
+Write-Host "All files OK" -ForegroundColor Green
