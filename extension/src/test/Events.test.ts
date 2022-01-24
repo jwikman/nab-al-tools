b7570873e071a3d2551786b58d5d9576dd1a618b
theschitz <theodore@theschitz.se>
2022-01-24 17:55:50 +0100

    assert cache was updated

--- extension/src/test/Events.test.ts
+++ extension/src/test/Events.test.ts
@@ -7,28 +7,41 @@ suite("Events Tests", () => {
   const cachedFilePath = path.join(
     __dirname,
     "../../",
-    "src/test/resources/NAB_AL_Tools.da-DK.xlf"
+    "src/test/resources/XliffCacheTest.da-DK copy.xlf"
   );
   test("NABFunctions.onDidChangeTextDocument", async function () {
+    const expectedTargetContent = "Lionel Richie";
+    const newText = `\n          <target>${expectedTargetContent}</target>`;
+    const startPosition = new vscode.Position(8, 37);
+    // Clear cache
+    xliffCache.clear();
     assert.ok(xliffCache.size === 0, "Cache is not empty.");
     const textDocument = await vscode.workspace.openTextDocument(
       cachedFilePath
     );
     const editor = await vscode.window.showTextDocument(textDocument);
     editor.edit((editBuilder) => {
-      editBuilder.insert(new vscode.Position(0, 0), "    ");
+      editBuilder.insert(startPosition, newText);
     });
     await textDocument.save();
-    assert.ok(xliffCache.get(cachedFilePath), "Document was not cached.");
+    const cachedXliff = xliffCache.get(cachedFilePath);
     assert.strictEqual(xliffCache.size, 1, "Expected 1 document in cache.");
-
+    assert.ok(cachedXliff, "Document was not cached.");
+    assert.strictEqual(
+      cachedXliff.transunit[0].target.textContent,
+      expectedTargetContent,
+      "Cached Xliff was not updated."
+    );
     // Clear cache
     xliffCache.clear();
 
     // Restore document
     editor.edit((editBuilder) => {
       editBuilder.delete(
-        new vscode.Range(new vscode.Position(0, 0), new vscode.Position(0, 4))
+        new vscode.Range(
+          new vscode.Position(startPosition.line, 37),
+          new vscode.Position(startPosition.line + 1, newText.length)
+        )
       );
     });
     await textDocument.save();
