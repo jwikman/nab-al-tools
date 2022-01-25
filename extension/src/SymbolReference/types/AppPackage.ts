52aa7ed03f8bdd6e3273b505c23966773fb31100
theschitz <theodore@theschitz.se>
2022-01-25 19:46:56 +0100

    refactor(AppPackage): remove unused sort function

--- extension/src/SymbolReference/types/AppPackage.ts
+++ extension/src/SymbolReference/types/AppPackage.ts
@@ -1,7 +1,7 @@
 import { ALObject } from "../../ALObject/ALElementTypes";
 import { ManifestPackage } from "../interfaces/NavxManifest";
 import { SymbolReference } from "../interfaces/SymbolReference";
-import * as Version from "../../helpers/Version";
+
 
 export class AppPackage {
   name: string;
@@ -35,20 +35,5 @@ export class AppPackage {
     if (symbolReference) {
       this.symbolReference = symbolReference;
     }
-  }
-
-  public sort(other: AppPackage): number {
-    if (this.publisher !== other.publisher) {
-      return this.publisher.localeCompare(other.publisher);
-    }
-    if (this.name !== other.name) {
-      return this.name.localeCompare(other.name);
-    }
-    if (Version.lt(other.version, this.version)) {
-      return -1;
-    } else if (Version.gt(other.version, this.version)) {
-      return 1;
-    }
-    return 0;
   }
 }
