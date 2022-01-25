bf067a6bfd2d5f96c3167398f65006800e68d932
theschitz <theodore@theschitz.se>
2022-01-25 21:02:38 +0100

    refactor: remove moved functions

--- extension/src/SymbolReference/SymbolReferenceReader.ts
+++ extension/src/SymbolReference/SymbolReferenceReader.ts
@@ -1,6 +1,3 @@
-import * as AdmZip from "adm-zip"; // Ref: https://www.npmjs.com/package/adm-zip
-import * as fs from "fs";
-import * as path from "path";
 import {
   ALObject,
   ALControl,
@@ -12,170 +9,32 @@ import {
   ControlKind,
   PageDefinition,
   SymbolProperty,
-  SymbolReference,
   TableDefinition,
 } from "./interfaces/SymbolReference";
 import { ALControlType, ALObjectType } from "../ALObject/Enums";
 import { ALTableField } from "../ALObject/ALTableField";
 import { alPropertyTypeMap, multiLanguageTypeMap } from "../ALObject/Maps";
-import * as txml from "txml";
-import { ManifestPackage, NavxManifest } from "./interfaces/NavxManifest";
 import { AppPackage } from "./types/AppPackage";
 import { symbolReferenceCache } from "./SymbolReferenceCache";
 import { ALPageField } from "../ALObject/ALPageField";
 import { ALPagePart } from "../ALObject/ALPagePart";
-import { BinaryReader } from "./BinaryReader";
-
-function getAppFileContent(
-  appFilePath: string,
-  loadSymbols = true
-): { symbolReference: string; manifest: string; packageId: string } {
-  let symbolReference = "";
-  let manifest = "";
-  const fileContent = fs.readFileSync(appFilePath);
-  const view = new BinaryReader(fileContent, true);
-
-  const magicNumber1 = view.getUint32(0);
-  const metadataSize = view.getUint32(4);
-  const metadataVersion = view.getUint32(8);
-
-  const packageIdArray = Buffer.from(view.getBytes(16, 12));
-  const byteArray: number[] = [];
-  packageIdArray.forEach((b) => byteArray.push(b));
-  const packageId = byteArrayToGuid(byteArray);
-  const contentLength = view.getUint64(28);
-  const magicNumber2 = view.getUint32(36);
-  const magicNumber3 = view.getUint16(40);
-
-  const appIdentifier = 0x5856414e; // "NAVX"
-  const runtimePackageIdentifier = 20014; // "."
-  const regularAppIdentifier = 19280; // "P"
-
-  if (
-    magicNumber1 !== appIdentifier ||
-    magicNumber2 !== appIdentifier ||
-    metadataVersion > 2
-  ) {
-    throw new Error(`"${appFilePath}" is not a valid app file`);
-  }
-
-  if (
-    magicNumber3 !== runtimePackageIdentifier &&
-    magicNumber3 !== regularAppIdentifier
-  ) {
-    throw new Error(
-      `Unsupported package format (unknown package container type in "${appFilePath})"`
-    );
-  }
-
-  if (magicNumber3 === runtimePackageIdentifier) {
-    // Runtime Package
-    throw new Error(`Runtime Packages is not supported (${appFilePath})`);
-  }
-
-  const buffer = Buffer.from(
-    view.getBytes(contentLength.valueOf(), metadataSize)
-  );
-
-  const zip = new AdmZip(buffer);
-  const zipEntries = zip.getEntries(); // an array of ZipEntry records
-  if (loadSymbols) {
-    symbolReference = getZipEntryContentOrEmpty(
-      zipEntries,
-      "SymbolReference.json"
-    );
-    symbolReference = symbolReference.replace(/\0/g, ""); // Trailing NULL characters seems to be common...
-  }
-  manifest = getZipEntryContentOrEmpty(zipEntries, "NavxManifest.xml");
-
-  return {
-    symbolReference: symbolReference,
-    manifest: manifest,
-    packageId: packageId,
-  };
-}
-
-function byteArrayToGuid(byteArray: number[]): string {
-  // reverse first four bytes, and join with following two reversed, joined with following two reversed, joined with rest of the bytes
-  byteArray = byteArray
-    .slice(0, 4)
-    .reverse()
-    .concat(byteArray.slice(4, 6).reverse())
-    .concat(byteArray.slice(6, 8).reverse())
-    .concat(byteArray.slice(8));
-
-  let guidValue = byteArray
-    .map(function (item) {
-      // return hex value with "0" padding
-      return ("00" + item.toString(16).toUpperCase()).substr(-2, 2);
-    })
-    .join("");
-  guidValue = `${guidValue.substr(0, 8)}-${guidValue.substr(
-    8,
-    4
-  )}-${guidValue.substr(12, 4)}-${guidValue.substr(16, 4)}-${guidValue.substr(
-    20
-  )}`;
-  return guidValue.toLowerCase();
-}
-
-function getZipEntryContentOrEmpty(
-  zipEntries: AdmZip.IZipEntry[],
-  fileName: string
-): string {
-  const zipEntry = zipEntries.filter(
-    (zipEntry) => zipEntry.name === fileName
-  )[0];
-  if (zipEntry === undefined) {
-    return "";
-  }
-  let fileContent = zipEntry.getData().toString("utf8");
-  if (fileContent.charCodeAt(0) === 0xfeff) {
-    // Remove BOM
-    fileContent = fileContent.slice(1);
-  }
-  return fileContent;
-}
-export function getAppPackage(
-  appFilePath: string,
-  loadSymbols = true
-): AppPackage {
-  const appFileContent = getAppFileContent(appFilePath);
-  let symbols: SymbolReference;
-  const manifest: ManifestPackage = (<NavxManifest>(
-    txml.simplifyLostLess(txml.parse(appFileContent.manifest) as txml.tNode[])
-  )).Package[0];
-  const appPackage: AppPackage = new AppPackage(
-    appFilePath,
-    manifest.App[0]._attributes.Name,
-    manifest.App[0]._attributes.Publisher,
-    manifest.App[0]._attributes.Version,
-    appFileContent.packageId,
-    manifest
-  );
-  if (loadSymbols) {
-    symbols = <SymbolReference>JSON.parse(appFileContent.symbolReference);
-    appPackage.symbolReference = symbols;
-  }
-  return appPackage;
-}
 
 export function getObjectsFromAppFile(appFilePath: string): AppPackage {
-  const appMeta = getAppIdentifiersFromFilename(appFilePath);
+  const appIdentifier = AppPackage.appIdentifier(appFilePath);
 
   let appPackage;
-  if (symbolReferenceCache.isCached(appMeta)) {
-    appPackage = symbolReferenceCache.get(appMeta);
+  if (symbolReferenceCache.isCached(appIdentifier)) {
+    appPackage = symbolReferenceCache.get(appIdentifier);
   }
   if (!appPackage) {
-    appPackage = getAppPackage(appFilePath);
+    appPackage = AppPackage.getAppPackage(appFilePath);
     parseObjectsInAppPackage(appPackage);
-    symbolReferenceCache.add(appPackage);
+    symbolReferenceCache.set(appPackage);
   }
   return appPackage;
 }
 
-export function parseObjectsInAppPackage(appPackage: AppPackage): void {
+function parseObjectsInAppPackage(appPackage: AppPackage): void {
   if (appPackage.symbolReference === undefined) {
     return;
   }
@@ -317,22 +176,4 @@ function addProperty(prop: SymbolProperty, obj: ALControl): void {
   } else if (alPropertyTypeMap.has(prop.Name.toLowerCase())) {
     obj.properties.push(new ALProperty(obj, 0, prop.Name, prop.Value));
   }
-}
-
-export function getAppIdentifiersFromFilename(
-  filePath: string
-): { valid: boolean; name: string; publisher: string; version: string } {
-  let fileName = path.basename(filePath);
-  const ext = path.extname(filePath);
-  fileName = fileName.slice(0, fileName.length - ext.length);
-  if (fileName.indexOf("_") > 0) {
-    const appParts = fileName.split("_");
-    return {
-      valid: true,
-      name: appParts[1],
-      publisher: appParts[0],
-      version: appParts[2],
-    };
-  }
-  return { valid: false, name: fileName, publisher: "", version: "" };
 }
