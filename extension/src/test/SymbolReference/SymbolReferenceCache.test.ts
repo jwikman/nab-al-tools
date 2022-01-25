a1348e6367189b27abd0c7c787a1d4daf70c183d
theschitz <theodore@theschitz.se>
2022-01-25 20:26:38 +0100

    test(SymbolReferenceCache): add tests

---
+++ extension/src/test/SymbolReference/SymbolReferenceCache.test.ts
@@ -0,0 +1,50 @@
+import * as path from "path";
+import * as assert from "assert";
+import { SymbolReferenceCache } from "../../SymbolReference/SymbolReferenceCache";
+import * as SymbolReferenceReader from "../../SymbolReference/SymbolReferenceReader";
+
+suite("Symbol Reference Cache", () => {
+  const testResourcesPath = "../../../src/test/resources/.alpackages";
+  const testAppPath = path.resolve(
+    __dirname,
+    testResourcesPath,
+    "Default publisher_Al_1.0.0.0.app"
+  );
+
+  const appPackage = SymbolReferenceReader.getObjectsFromAppFile(testAppPath);
+  test("SymbolReferenceCache.get()", function () {
+    const cache = new SymbolReferenceCache();
+    cache.set(appPackage);
+    assert.strictEqual(cache.size, 1);
+    assert.deepStrictEqual(cache.get(appPackage), appPackage);
+  });
+
+  test("SymbolReferenceCache.set()", function () {
+    const cache = new SymbolReferenceCache();
+    cache.set(appPackage);
+    assert.strictEqual(cache.size, 1);
+    assert.ok(cache.isCached(appPackage));
+  });
+
+  test("SymbolReferenceCache.isCached()", function () {
+    const cache = new SymbolReferenceCache();
+    cache.set(appPackage);
+    assert.ok(cache.isCached(appPackage));
+  });
+
+  test("SymbolReferenceCache.delete()", function () {
+    const cache = new SymbolReferenceCache();
+    cache.set(appPackage);
+    assert.strictEqual(cache.size, 1);
+    assert.ok(cache.delete(appPackage));
+    assert.strictEqual(cache.size, 0);
+  });
+
+  test("SymbolReferenceCache.clear()", function () {
+    const cache = new SymbolReferenceCache();
+    cache.set(appPackage);
+    assert.strictEqual(cache.size, 1);
+    cache.clear();
+    assert.strictEqual(cache.size, 0);
+  });
+});
