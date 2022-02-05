c18468f279d188c1d972274982240e3e3c2b1e0d
theschitz <theodore@theschitz.se>
2022-02-05 18:51:43 +0100

    test(vscodeFunctions): add test for optional parameter

--- extension/src/test/VSCodeFunctions.test.ts
+++ extension/src/test/VSCodeFunctions.test.ts
@@ -2,9 +2,15 @@ import * as assert from "assert";
 import * as VSCodeFunctions from "../VSCodeFunctions";
 
 suite("VSCodeFunctions", function () {
-  test("findTextFiles()", async function () {
+  test("findTextFiles() - optional param filesToInclude", async function () {
     await assert.doesNotReject(async () => {
       VSCodeFunctions.findTextInFiles("table", false, ".md");
+    }, "Unexpected rejection of promise");
+  });
+
+  test("findTextFiles()", async function () {
+    await assert.doesNotReject(async () => {
+      VSCodeFunctions.findTextInFiles("table", false);
     }, "Unexpected rejection of promise");
   });
 });
