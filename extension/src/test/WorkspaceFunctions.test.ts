73bc412b8b4678fe1dc69a5c84eaceb29097c635
theschitz <17023248+theschitz@users.noreply.github.com>
2022-02-07 14:15:05 +0100

    WIP test getDtsOutputFiles

--- extension/src/test/WorkspaceFunctions.test.ts
+++ extension/src/test/WorkspaceFunctions.test.ts
@@ -1,5 +1,7 @@
-import * as assert from "assert";
-import * as WorkspaceFunctions from "../WorkspaceFunctions";
-suite("Workspace Functions", function () {
-  test("getDtsOutputFiles(): Error", function () {});
-});
+// import * as assert from "assert";
+// import * as WorkspaceFunctions from "../WorkspaceFunctions";
+// suite("Workspace Functions", function () {
+//   test("getDtsOutputFiles(): Error", function () {
+//     this.skip();
+//   });
+// });
