71d12e85df7fae09b5aabb16f04ddfccee9a55bf
theschitz <17023248+theschitz@users.noreply.github.com>
2022-02-17 22:16:04 +0100

    better regex ftw

--- extension/src/externalresources/BaseAppTranslationFiles.ts
+++ extension/src/externalresources/BaseAppTranslationFiles.ts
@@ -3,7 +3,7 @@ import { resolve, basename } from "path";
 
 import { BlobContainer } from "./ExternalResources";
 
-const languageCodeJsonRE = new RegExp(/([a-z]{2}-[a-z]{2}(_\w*)?).json/gi);
+const languageCodeJsonRE = new RegExp(/^([a-z]{2}-[a-z]{2}(_\w*)?)\.json$/i);
 export class BlobContainerSettings {
   public static sasToken =
     "sv=2020-08-04&ss=f&srt=o&sp=r&se=2025-11-01T19:00:00Z&st=2021-11-24T11:00:00Z&spr=https&sig=sxDvahZ%2FPxuuuMwriMiBHWI6E%2FSjQkz6pUSABNvyjak%3D";
