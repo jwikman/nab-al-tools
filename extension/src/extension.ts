0b2ed23b38de55338e1ccf882bbe7fc6e7df409b
Johannes Wikman <jwikman@users.noreply.github.com>
2022-02-20 01:39:18 +0100

    feat: Troubleshooting - Find TransUnits without source (#298)
    * test: Compile TestApp
    
    * test: Added captions to TestApp
    
    * feat: Troubleshoot Find TransUnits w/o source
    
    * Moved to Troubleshooting.ts
    
    * CR fixes
    
    * Moved showErrorAndLog to VSCodeFunctions
    
    * Modal error messages
    
    * grouped troubleshooting commands
    
    * Improved serialization error handling

--- extension/src/extension.ts
+++ extension/src/extension.ts
@@ -2,6 +2,7 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 import * as NABfunctions from "./NABfunctions"; //Our own functions
+import * as Troubleshooting from "./Troubleshooting"; //Our own functions
 import * as DebugTests from "./DebugTests";
 import * as SettingsLoader from "./Settings/SettingsLoader";
 import { XlfHighlighter } from "./XlfHighlighter";
@@ -25,7 +26,7 @@ export function activate(context: vscode.ExtensionContext): void {
   // The command has been defined in the package.json file
   // The commandId parameter must match the command field in package.json
 
-  const commandlist = [
+  const commandList = [
     vscode.commands.registerCommand("nab.RefreshXlfFilesFromGXlf", () => {
       NABfunctions.refreshXlfFilesFromGXlf();
     }),
@@ -155,12 +156,6 @@ export function activate(context: vscode.ExtensionContext): void {
     vscode.commands.registerCommand("nab.renumberALObjects", () => {
       NABfunctions.renumberALObjects();
     }),
-    vscode.commands.registerCommand("nab.troubleshootParseCurrentFile", () => {
-      NABfunctions.troubleshootParseCurrentFile();
-    }),
-    vscode.commands.registerCommand("nab.troubleshootParseAllFiles", () => {
-      NABfunctions.troubleshootParseAllFiles();
-    }),
     vscode.commands.registerTextEditorCommand(
       "nab.AddXmlCommentBold",
       (textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit) => {
@@ -220,7 +215,23 @@ export function activate(context: vscode.ExtensionContext): void {
     }),
   ];
 
-  context.subscriptions.concat(commandlist);
+  const troubleshootingFunctions = [
+    vscode.commands.registerCommand("nab.troubleshootParseCurrentFile", () => {
+      Troubleshooting.troubleshootParseCurrentFile();
+    }),
+    vscode.commands.registerCommand("nab.troubleshootParseAllFiles", () => {
+      Troubleshooting.troubleshootParseAllFiles();
+    }),
+    vscode.commands.registerCommand(
+      "nab.troubleshootFindTransUnitsWithoutSource",
+      () => {
+        Troubleshooting.troubleshootFindTransUnitsWithoutSource();
+      }
+    ),
+  ];
+
+  context.subscriptions.concat(commandList);
+  context.subscriptions.concat(troubleshootingFunctions);
   //context.subscriptions.push(disposable);
 }
 
