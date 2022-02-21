459318e69df7384e89986882b9b2f79bcf83d827
Johannes Wikman <jwikman@users.noreply.github.com>
2022-02-21 20:45:22 +0100

    Tests for grid on page

--- extension/src/test/ALObjectTestLibrary.ts
+++ extension/src/test/ALObjectTestLibrary.ts
@@ -1635,6 +1635,50 @@ page 50000 "NAB Test Table Card"
         asdf: Option;
 }`;
 }
+export function getPageWithGrid(): string {
+  return `
+page 50006 "NAB With Grid"
+{
+    Caption = 'Page With Grid';
+    DeleteAllowed = false;
+    InsertAllowed = false;
+    ModifyAllowed = false;
+    PageType = Worksheet;
+    SourceTable = "Item";
+    SourceTableTemporary = true;
+    UsageCategory = None;
+
+    layout
+    {
+        area(content)
+        {
+            grid(Option)
+            {
+                Caption = 'Option';
+                field(ItemFilter; ItemFilter)
+                {
+                    ApplicationArea = Assembly;
+                    Caption = 'Item Filter';
+                    ToolTip = 'Specifies the bla bla bla.';
+                }
+            }
+            repeater(Group)
+            {
+                Caption = 'Lines';
+                ShowAsTree = true;
+                field(Type; Rec.Type)
+                {
+                    Caption = 'Type';
+                    ApplicationArea = Assembly;
+                }
+            }
+        }
+    }
+    var
+        ItemFilter: Text;
+}
+`;
+}
 
 export function getPageWithToolTips(): string {
   return `
