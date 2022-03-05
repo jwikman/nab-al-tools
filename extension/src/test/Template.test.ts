1b92b93a7842902436485270bb49c99bc61ab597
theschitz <17023248+theschitz@users.noreply.github.com>
2022-03-05 14:05:13 +0100

    test deserialization of postConversionTasks

--- extension/src/test/Template.test.ts
+++ extension/src/test/Template.test.ts
@@ -60,6 +60,11 @@ suite("Template", function () {
       2,
       "Unexpected createXlfLanguages.length"
     );
+    assert.strictEqual(
+      templateSettings.postConversionTasks.length,
+      1,
+      "Unexpected number of postConversionTasks"
+    );
   });
 
   test("Parse Template Settings: Error - Path does not exist", function () {
