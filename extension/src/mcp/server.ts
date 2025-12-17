653439972a49e1e4f7b443cfe72edc0f60000394
jwikman <jwikman@users.noreply.github.com>
2025-12-17 10:44:21 +0000

    chore: v1.45.512171043

--- extension/src/mcp/server.ts
+++ extension/src/mcp/server.ts
@@ -77,7 +77,7 @@ function handleMcpToolError(
 }
 
 export const mcpServerId = "nab-al-tools-mcp-server";
-export const mcpServerVersion = "1.45.512081951";
+export const mcpServerVersion = "1.45.512171043";
 export const mcpServerTitle = "NAB AL Tools MCP Server";
 
 /**
